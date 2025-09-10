7ab66a2016f8bf5ab5c2db5cfb192b01a55b74eb
SameerPanda06 <129105035+SameerPanda06@users.noreply.github.com>
2025-09-10 22:51:15 +0530

    Delete SchEdu directory

--- SchEdu/backend/routes/protected.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authenticateToken = require('../middleware/authMiddleware');
-
-router.get('/dashboard', authenticateToken, (req, res) => {
-  res.json({ message: `Welcome ${req.user.email}, your role is ${req.user.role}` });
-});
-
-module.exports = router;
